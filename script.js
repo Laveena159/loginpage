1a156daaee685bf2fbe3e7e65f3274be853ea50f
Laveena159 <laveenagowri@gmail.com>
2024-07-15 14:46:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("loginbtn");
+document.getElementById("registerLink");
+document.getElementById("Registerbtn");
+document.getElementById("loginlink");
+
+function Register() {
+  document.querySelector(".page").style.display = "none";
+  document.querySelector(".registerpage").style.display = "block";
+}
+
+function Login() {
+  document.querySelector(".registerpage").style.display = "none";
+  document.querySelector(".page").style.display = "block";
+}
+function forgetpassword() {
+  document.querySelector(".");
+}
